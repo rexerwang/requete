4ffde29b0863b930124e2b7f517a42e8fe8248b0
rexerwang <rexercn@gmail.com>
2023-04-29 20:01:16 +0800

    chore(examples): fix cjs assert

---
+++ examples/cjs/README.md
@@ -0,0 +1,9 @@
+# cjs
+
+test commonjs bundle imports
+
+Run install & test:
+
+```sh
+pnpm it
+```
--- examples/cjs/index.cjs
+++ examples/cjs/index.cjs
@@ -1,20 +1,28 @@
 const assert = require('assert/strict')
 
 const requete = require('requete')
-assert(typeof requete, 'function')
-assert(typeof requete.RequestError, 'function')
-assert(typeof requete.Requete, 'function')
-assert(typeof requete.TimeoutAbortController, 'function')
-assert(typeof requete.create, 'function')
-assert(typeof requete.default, 'function')
+assert.equal(typeof requete, 'object', 'requete')
+assert.equal(typeof requete.RequestError, 'function', 'requete.RequestError')
+assert.equal(typeof requete.Requete, 'function', 'requete.Requete')
+assert.equal(
+  typeof requete.TimeoutAbortController,
+  'function',
+  'requete.TimeoutAbortController'
+)
+assert.equal(typeof requete.create, 'function', 'requete.create')
 
 const { logger } = require('requete/middleware')
-assert(typeof logger, 'function')
+assert.equal(typeof logger, 'function', 'requete/middleware:logger')
 
-const { Adapter, FetchAdapter, XhrAdapter, create } = require('requete/adapter')
-assert(typeof Adapter, 'function')
-assert(typeof FetchAdapter, 'function')
-assert(typeof XhrAdapter, 'function')
-assert(typeof create, 'function')
+const {
+  Adapter,
+  FetchAdapter,
+  XhrAdapter,
+  createAdapter,
+} = require('requete/adapter')
+assert.equal(typeof Adapter, 'function', 'requete/adapter:Adapter')
+assert.equal(typeof FetchAdapter, 'function', 'requete/adapter:FetchAdapter')
+assert.equal(typeof XhrAdapter, 'function', 'requete/adapter:XhrAdapter')
+assert.equal(typeof createAdapter, 'function', 'requete/adapter:createAdapter')
 
 console.log('All tests passed.')

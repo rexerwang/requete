40b442692843d0706eb30f0f4e8e37c74bf7f00e
rexerwang <rexercn@gmail.com>
2023-05-06 19:13:11 +0800

    feat(build): add file `index.browser.mjs` for browser env

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -15,9 +15,10 @@ const commonPlugins = [
 ]
 
 const dist = (file = '') => path.join('dist', file)
-const min = (file) => {
+
+const dotWith = (text) => (file) => {
   const ext = path.extname(file)
-  return file.slice(0, -ext.length) + '.min' + ext
+  return file.slice(0, -ext.length) + '.' + text + ext
 }
 
 const useBabel = () =>
@@ -26,81 +27,8 @@ const useBabel = () =>
     presets: ['@babel/preset-env'],
   })
 
-const setupModuleConfig = (file, external = /^requete/, exports = null) => {
-  const module = pkg.exports[file === 'index' ? '.' : `./${file}`]
-  const input = `src/${file}.ts`
-
-  return [
-    {
-      input,
-      external,
-      output: {
-        file: dist(module.import),
-        format: 'es',
-        generatedCode: { constBindings: true },
-        banner,
-      },
-      plugins: commonPlugins,
-    },
-    {
-      input,
-      external,
-      output: {
-        file: dist(module.require),
-        format: 'cjs',
-        esModule: false,
-        exports: 'named',
-        outro: exports
-          ? [
-              `module.exports = ${exports.default};`,
-              ...Object.entries(exports)
-                .filter(([key]) => key !== 'default')
-                .map(([key, value]) => `module.exports.${key} = ${value};`),
-              'exports.default = module.exports;',
-            ].join('\n')
-          : '',
-        generatedCode: { constBindings: true },
-        banner,
-      },
-      plugins: commonPlugins.concat(useBabel()),
-    },
-    // define declaration
-    ...(file === 'index'
-      ? [
-          {
-            input,
-            external,
-            output: {
-              dir: path.dirname(dist(pkg.types)),
-              format: 'es',
-              banner,
-            },
-            plugins: [
-              typescript({
-                rootDir: 'src',
-                exclude: ['**/*.{test,spec}.ts'],
-                declaration: true,
-                emitDeclarationOnly: true,
-                outDir: path.dirname(dist(pkg.types)),
-              }),
-              generatePackageJson({ src: pkg, dest: dist() }),
-              copy([
-                { src: 'LICENSE', dest: dist() },
-                { src: 'README.md', dest: dist() },
-                { src: '.npmignore', dest: dist() },
-              ]),
-            ],
-          },
-        ]
-      : []),
-  ]
-}
-
-const setupUMDConfig = (config) => {
-  config.output.format = 'umd'
-  config.output.banner = banner
-  config.plugins = commonPlugins.concat(config.plugins ?? [], [useBabel()])
-
+const withMinify = (config) => {
+  const min = dotWith('min')
   return [
     config,
     {
@@ -109,6 +37,96 @@ const setupUMDConfig = (config) => {
       output: { ...config.output, file: min(config.output.file) },
     },
   ]
+}
+
+const setupModuleConfig = (file, external = /^requete/, exports = null) => {
+  const module = pkg.exports[file === 'index' ? '.' : `./${file}`]
+  const input = `src/${file}.ts`
+
+  // define esm
+  const mjs = {
+    input,
+    external,
+    output: {
+      file: dist(module.import),
+      format: 'es',
+      generatedCode: { constBindings: true },
+      banner,
+    },
+    plugins: commonPlugins,
+  }
+
+  // define commonjs
+  const cjs = {
+    input,
+    external,
+    output: {
+      file: dist(module.require),
+      format: 'cjs',
+      esModule: false,
+      exports: 'named',
+      outro: exports
+        ? [
+            `module.exports = ${exports.default};`,
+            ...Object.entries(exports)
+              .filter(([key]) => key !== 'default')
+              .map(([key, value]) => `module.exports.${key} = ${value};`),
+            'exports.default = module.exports;',
+          ].join('\n')
+        : '',
+      generatedCode: { constBindings: true },
+      banner,
+    },
+    plugins: commonPlugins.concat(useBabel()),
+  }
+
+  const configs = [mjs, cjs]
+
+  if (file === 'index') {
+    configs.push(
+      // define browser esm
+      ...withMinify({
+        ...mjs,
+        external: null,
+        output: { ...mjs.output, file: dotWith('browser')(mjs.output.file) },
+      }),
+      // define declaration
+      {
+        input,
+        external,
+        output: {
+          dir: path.dirname(dist(pkg.types)),
+          format: 'es',
+          banner,
+        },
+        plugins: [
+          typescript({
+            rootDir: 'src',
+            exclude: ['**/*.{test,spec}.ts'],
+            declaration: true,
+            emitDeclarationOnly: true,
+            outDir: path.dirname(dist(pkg.types)),
+          }),
+          generatePackageJson({ src: pkg, dest: dist() }),
+          copy([
+            { src: 'LICENSE', dest: dist() },
+            { src: 'README.md', dest: dist() },
+            { src: '.npmignore', dest: dist() },
+          ]),
+        ],
+      }
+    )
+  }
+
+  return configs
+}
+
+const setupUMDConfig = (config) => {
+  config.output.format = 'umd'
+  config.output.banner = banner
+  config.plugins = commonPlugins.concat(config.plugins ?? [], [useBabel()])
+
+  return withMinify(config)
 }
 
 export default [].concat(

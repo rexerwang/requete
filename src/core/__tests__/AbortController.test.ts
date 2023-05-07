a52f3e4f208df9d3ea37fbe568631abb5a0eb386
rexerwang <rexercn@gmail.com>
2023-05-07 12:55:09 +0800

    test: toString spec of TimeoutAbortController

--- src/core/__tests__/AbortController.test.ts
+++ src/core/__tests__/AbortController.test.ts
@@ -1,11 +1,22 @@
 import { TimeoutAbortController } from '../AbortController'
 
-it('should thrown a ReferenceError when AbortController not supported', () => {
-  // @ts-ignore
-  TimeoutAbortController.supported = false
+describe('TimeoutAbortController specs', () => {
+  it('should thrown a ReferenceError when AbortController not supported', () => {
+    // @ts-ignore
+    TimeoutAbortController.supported = false
 
-  expect(() => new TimeoutAbortController(0)).toThrow(ReferenceError)
-  expect(() => new TimeoutAbortController(0)).toThrow(
-    'Not support AbortController in current environment.'
-  )
+    expect(() => new TimeoutAbortController(0)).toThrow(ReferenceError)
+    expect(() => new TimeoutAbortController(0)).toThrow(
+      'Not support AbortController in current environment.'
+    )
+
+    // @ts-ignore
+    TimeoutAbortController.supported = true
+  })
+
+  it('should return [object TimeoutAbortController] when called with Object.prototype.toString', () => {
+    expect(Object.prototype.toString.call(new TimeoutAbortController(0))).toBe(
+      '[object TimeoutAbortController]'
+    )
+  })
 })
